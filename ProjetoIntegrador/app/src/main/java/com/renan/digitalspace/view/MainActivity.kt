4c96331d8da9431f7e39fb48dc3c5b476d199b28
Renan <jrferreirad@gmail.com>
2020-11-14 18:01:46 -0300

    Adicionando projeto base e implementando navigation

---
+++ ProjetoIntegrador/app/src/main/java/com/renan/digitalspace/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.renan.digitalspace.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.renan.digitalspace.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
