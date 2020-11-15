5e83cec22116cf335227edddae484b4fe34c9283
Renan <jrferreirad@gmail.com>
2020-11-15 02:49:03 -0300

    criação tela APOD e mapeamento da API mais implementação do Retrofit para gerenciamento de dados

---
+++ app/src/main/java/com/renan/digitalspace/model/ApiResponseModelAPOD.kt
@@ -0,0 +1,10 @@
+package com.renan.digitalspace.model
+
+data class ApiResponseModelAPOD(
+    val copyright: String?,
+    val date: String,
+    val explanation:String,
+    val title:String,
+    val url:String
+
+)
--- app/src/main/java/com/renan/digitalspace/view/MainActivity.kt
+++ app/src/main/java/com/renan/digitalspace/view/MainActivity.kt
@@ -2,11 +2,37 @@ package com.renan.digitalspace.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.renan.digitalspace.NetworkUtils
 import com.renan.digitalspace.R
+import com.renan.digitalspace.model.ApiResponseModelAPOD
+import com.renan.digitalspace.repository.EndPoint
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val remote = NetworkUtils.createService(EndPoint::class.java)
+        val call: Call<ApiResponseModelAPOD> = remote.getAstronomicalFact()
+
+        val response = call.enqueue(object : Callback<ApiResponseModelAPOD>{
+            override fun onResponse(
+                call: Call<ApiResponseModelAPOD>,
+                response: Response<ApiResponseModelAPOD>
+            ) {
+                val s = response.body()
+            }
+
+            override fun onFailure(call: Call<ApiResponseModelAPOD>, t: Throwable) {
+
+                val s = t.message
+            }
+
+        })
+
     }
 }
