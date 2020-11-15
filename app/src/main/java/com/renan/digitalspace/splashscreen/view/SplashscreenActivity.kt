a2121cd6ddf0dad76740d583a686b827fd6eed37
paulo4fs <engpaulofrota@gmail.com>
2020-11-15 07:36:29 -0300

    package da splashscreen alterado e adicionado nohistory no manifest da splashscreen

--- app/src/main/java/com/renan/digitalspace/splashscreen/view/SplashscreenActivity.kt
+++ app/src/main/java/com/renan/digitalspace/splashscreen/view/SplashscreenActivity.kt
@@ -1,4 +1,4 @@
-package com.renan.digitalspace.view
+package com.renan.digitalspace.splashscreen.view
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.renan.digitalspace.R
+import com.renan.digitalspace.view.MainActivity
 
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
