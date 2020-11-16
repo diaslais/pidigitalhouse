8f69adb84c6457a5cbae8d6ea90cacb458ee043c
paulo4fs <engpaulofrota@gmail.com>
2020-11-16 16:51:19 -0300

    Validacao dos campos do LoginFragment criada

--- app/src/main/java/com/renan/digitalspace/login/view/LoginFragment.kt
+++ app/src/main/java/com/renan/digitalspace/login/view/LoginFragment.kt
@@ -2,16 +2,21 @@ package com.renan.digitalspace.login.view
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.view.ContextThemeWrapper
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.renan.digitalspace.R
 import kotlinx.android.synthetic.main.fragment_login.*
 
@@ -22,8 +27,6 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-
         val contextThemeWrapper: Context = ContextThemeWrapper(activity, R.style.AppTheme_Login)
 
         // clone the inflater using the ContextThemeWrapper
@@ -39,11 +42,59 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val navController = Navigation.findNavController(view)
         view.findViewById<MaterialButton>(R.id.mbLoginLogin).setOnClickListener {
-            navController.navigate(R.id.fatoAstronomicoFragment2)
+            errorHandler(view)
+            navigateLogin(view, navController)
         }
         navigateSignup(view, navController, R.id.imEmailLogin)
         navigateSignup(view, navController, R.id.imFacebookLogin)
         navigateSignup(view, navController, R.id.imGoogleLogin)
+    }
+
+    private fun navigateLogin(view: View, navController: NavController) {
+        val email = view.findViewById<TextInputEditText>(R.id.tietEmailLogin)
+        val password = view.findViewById<TextInputEditText>(R.id.tietPasswordLogin)
+
+        if (email.text.toString().isEmpty() || password.text.toString().isEmpty()) {
+            val toast =
+                Toast.makeText(context, getString(R.string.preencha_campos), Toast.LENGTH_LONG)
+            toast.show()
+        } else {
+            navController.navigate(R.id.fatoAstronomicoFragment2)
+        }
+    }
+
+    private fun errorHandler(view: View?) {
+        val emailText = view?.findViewById<TextInputEditText>(R.id.tietEmailLogin)
+        val emailLayout = view?.findViewById<TextInputLayout>(R.id.tilEmailLogin)
+        val passwordText = view?.findViewById<TextInputEditText>(R.id.tietPasswordLogin)
+        val passwordLayout = view?.findViewById<TextInputLayout>(R.id.tilPasswordLogin)
+
+        putError(emailText, emailLayout)
+        clearError(emailText, emailLayout)
+        putError(passwordText, passwordLayout)
+        clearError(passwordText, passwordLayout)
+    }
+
+    private fun putError(text: TextInputEditText?, layout: TextInputLayout?) {
+        if (text?.text.toString().isEmpty()) {
+            layout?.isErrorEnabled = true
+            layout?.error = getString(R.string.error_vazio)
+        }
+    }
+
+    private fun clearError(text: TextInputEditText?, layout: TextInputLayout?) {
+        text?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                layout!!.isErrorEnabled = false
+                layout.error = ""
+            }
+        })
     }
 
     private fun navigateSignup(view: View, navController: NavController, button: Int) {
