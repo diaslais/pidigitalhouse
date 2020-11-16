0c748e996643e56b126cd103ec06b33724155d07
paulo4fs <engpaulofrota@gmail.com>
2020-11-16 15:56:23 -0300

    layout signupFragment finalizado

--- app/src/main/java/com/renan/digitalspace/signup/view/SignupFragment.kt
+++ app/src/main/java/com/renan/digitalspace/signup/view/SignupFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.google.android.material.button.MaterialButton
 import com.renan.digitalspace.R
 
 class SignupFragment : Fragment() {
@@ -20,6 +22,14 @@ class SignupFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signup, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = Navigation.findNavController(view)
+        view.findViewById<MaterialButton>(R.id.mbLoginSignup).setOnClickListener {
+            navController.navigate(R.id.loginFragment)
+        }
     }
 
 }
