0074417c535fbcb4af13a2c221ce02035072fdb2
Lais Dias <laisdias099@gmail.com>
2020-11-19 17:29:14 -0300

    adiciona data class de planetas

--- app/src/main/java/com/renan/digitalspace/planetsmenu/model/Planet.kt
+++ app/src/main/java/com/renan/digitalspace/planetsmenu/model/Planet.kt
@@ -1,5 +1,10 @@
 package com.renan.digitalspace.planetsmenu.model
 
 data class Planet (
-    val name: String
+    val image: Int,
+    val name: String,
+    val description: String,
+    val technicalInformation: String,
+    val curiosities: String,
+    val news: String
 )
