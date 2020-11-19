da3e67a1b87537105ca3595d97b6779fe291a77d
Lais Dias <laisdias099@gmail.com>
2020-11-19 17:38:10 -0300

    adapter da recyclerview do menu planetas

---
+++ app/src/main/java/com/renan/digitalspace/planetsmenu/view/PlanetsMenuAdapter.kt
@@ -0,0 +1,20 @@
+package com.renan.digitalspace.planetsmenu.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.renan.digitalspace.R
+import com.renan.digitalspace.planetsmenu.model.Planet
+
+class PlanetsMenuAdapter (private val dataSet: List<Planet>, val clickListener: IPlanetClick): RecyclerView.Adapter<PlanetsMenuViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlanetsMenuViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_planets_menu, parent, false)
+        return PlanetsMenuViewHolder(view)
+    }
+
+    override fun getItemCount() = dataSet.size
+
+    override fun onBindViewHolder(holder: PlanetsMenuViewHolder, position: Int) {
+        holder.bind(dataSet[position], clickListener)
+    }
+}
