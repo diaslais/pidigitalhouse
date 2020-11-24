d5306d1e169566c38992732fee6823ff0d958cb8
Renan <jrferreirad@gmail.com>
2020-11-24 16:05:41 -0300

    ajustes para melhor funcionamento API apod

--- app/src/main/java/com/renan/digitalspace/apod/repository/ApodEndPoint.kt
+++ app/src/main/java/com/renan/digitalspace/apod/repository/ApodEndPoint.kt
@@ -4,14 +4,29 @@ import com.renan.digitalspace.apod.data.NetworkUtilsApod
 import com.renan.digitalspace.apod.model.ApodResponseModel
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.text.SimpleDateFormat
+import java.util.*
 
 interface ApodEndPoint {
 
-    @GET("planetary/apod")
-   suspend fun getAstronomicalFact(@Query("api_key") api_key: String = "gAFkqlGusIgjAzp0UrMhjbkqYfzlyq5jRL8BeBjv"): ApodResponseModel
+//    val date = Calendar.getInstance().time
+//
+//    val dateTimeFormatter = SimpleDateFormat("yyyy-dd-MM", Locale.getDefault())
+//
+//    return dateTimeFormatter.format(date)
 
-    companion object{
-        val endpoint: ApodEndPoint by lazy{
+
+
+
+
+    @GET("planetary/apod")
+    suspend fun getAstronomicalFact(
+        @Query("api_key") api_key: String = "gAFkqlGusIgjAzp0UrMhjbkqYfzlyq5jRL8BeBjv",
+        @Query("date") date: String = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Calendar.getInstance().time)
+    ): ApodResponseModel
+
+    companion object {
+        val endpoint: ApodEndPoint by lazy {
             NetworkUtilsApod.getRetroFitInstance().create(ApodEndPoint::class.java)
         }
     }
