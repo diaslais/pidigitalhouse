0a3aa89fd2d3e3eca1a0af8e9f824dbc1fcb99ad
paulo4fs <engpaulofrota@gmail.com>
2020-12-02 14:18:31 -0300

    criado navigation drawer, criado changeNameFragment dialog, e a lógica básica

--- app/src/main/java/com/renan/digitalspace/exploration/view/ExploracaoFragment.kt
+++ app/src/main/java/com/renan/digitalspace/exploration/view/ExploracaoFragment.kt
@@ -1,11 +1,13 @@
 package com.renan.digitalspace.exploration.view
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.button.MaterialButton
 import com.renan.digitalspace.R
@@ -28,8 +30,9 @@ class ExploracaoFragment : Fragment() {
 
         val navController = findNavController()
 
-        view.findViewById<MaterialButton>(R.id.btnSair).setOnClickListener {
-            navController.navigate(R.id.action_exploracaoFragment_to_loginFragment)
+        view.findViewById<MaterialButton>(R.id.ibNavigationExploracao).setOnClickListener {
+            val drawerLayout = activity?.findViewById<DrawerLayout>(R.id.drawer_layout)
+            drawerLayout?.openDrawer(Gravity.LEFT)
         }
         view.findViewById<MaterialButton>(R.id.btnExplorar).setOnClickListener {
             navController.navigate(R.id.action_exploracaoFragment_to_viaLacteaFragment)
