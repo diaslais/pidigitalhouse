e9040651f8d2139535594beff27563cf1e623eb6
Mariana Marcelli <marimmarcelli@gmail.com>
2020-12-08 16:50:17 -0300

    Implementando telas quiz e barra inferior Menu exploração

---
+++ app/src/main/java/com/renan/digitalspace/quiz/view/QuizQuestionsFragment.kt
@@ -0,0 +1,27 @@
+package com.renan.digitalspace.quiz.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.renan.digitalspace.R
+import com.renan.digitalspace.quiz.model.QuestionModel
+
+class QuizQuestionsFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz_questions, container, false)
+
+
+    }
+}
+
