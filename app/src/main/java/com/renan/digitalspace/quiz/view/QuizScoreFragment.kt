e83a16e2a8cda12159adbd01ee1ba4a3ef96e5cb
Mariana Marcelli <marimmarcelli@gmail.com>
2020-12-10 12:55:16 -0300

    Quiz Pontuação

--- app/src/main/java/com/renan/digitalspace/quiz/view/QuizScoreFragment.kt
+++ app/src/main/java/com/renan/digitalspace/quiz/view/QuizScoreFragment.kt
@@ -9,19 +9,19 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.button.MaterialButton
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.card.MaterialCardView
 import com.renan.digitalspace.R
 
 
-class QuizFragment : Fragment() {
+class QuizScoreFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
-
+        return inflater.inflate(R.layout.fragment_quiz_score, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -29,13 +29,8 @@ class QuizFragment : Fragment() {
 
         val navController = findNavController()
 
-        view.findViewById<MaterialButton>(R.id.btnPlay).setOnClickListener {
-            navController.navigate(R.id.action_quizFragment_to_quizQuestionsFragment)
+        view.findViewById<ImageButton>(R.id.btnExitToExploration).setOnClickListener {
+            navController.navigate(R.id.action_quizScoreFragment_to_explorationFragment)
         }
-
-        view.findViewById<ImageButton>(R.id.btnBackQuiz).setOnClickListener {
-            activity?.onBackPressed()
-        }
-
     }
 }
