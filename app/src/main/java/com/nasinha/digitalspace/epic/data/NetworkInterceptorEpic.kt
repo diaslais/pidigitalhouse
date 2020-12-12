4ccaf8cfaff86361a9fafaf487dd4162c264b75b
Renan <jrferreirad@gmail.com>
2020-12-12 15:34:54 -0300

    Adicionando interceptor na epic

---
+++ app/src/main/java/com/nasinha/digitalspace/epic/data/NetworkInterceptorEpic.kt
@@ -0,0 +1,30 @@
+package com.nasinha.digitalspace.epic.data
+
+import com.nasinha.digitalspace.apod.network.NetwokInterceptor
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class NetworkInterceptorEpic : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        val apikey = PRIVATE_KEY
+
+        var request = chain.request()
+        val originalHttpUrl = request.url()
+
+        val url = originalHttpUrl.newBuilder()
+            .addQueryParameter(API_KEY, apikey)
+            .build()
+
+        val requestBuilder = request.newBuilder().url(url)
+        request = requestBuilder.build()
+        return chain.proceed(request)
+    }
+
+    companion object {
+
+        private const val API_KEY = "api_key"
+        private const val PRIVATE_KEY = ""
+
+    }
+}
