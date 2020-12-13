279e12c31a51ea0cc64e78d7ed2f3059e658c08e
Renan <jrferreirad@gmail.com>
2020-12-13 16:05:14 -0300

    tratativas e avisos de risco de tradução e ajustes do sistema de tradução

--- app/src/main/java/com/nasinha/digitalspace/dialogs/view/SettingsFragment.kt
+++ app/src/main/java/com/nasinha/digitalspace/dialogs/view/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.nasinha.digitalspace.dialogs.view
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,15 +8,22 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Switch
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
+import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.google.android.material.button.MaterialButton
 import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.TranslatorOptions
 import com.nasinha.digitalspace.R
+import java.nio.channels.Selector
 
 
 class SettingsFragment : DialogFragment() {
@@ -30,15 +38,17 @@ class SettingsFragment : DialogFragment() {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         _view = view
+
         val switchButton = _view.findViewById<SwitchCompat>(R.id.btnSwitchTranslate)
 
         val prefs = activity?.getSharedPreferences(APP_NAME, AppCompatActivity.MODE_PRIVATE)
 
-        val prefsChecked = prefs?.getBoolean(NOTIFICATION_PREFS,false)
+        val prefsChecked = prefs?.getBoolean(NOTIFICATION_PREFS, false)
 
         if (prefsChecked != null) {
             switchButton.isChecked = prefsChecked
@@ -48,8 +58,8 @@ class SettingsFragment : DialogFragment() {
         confirmButton()
 
         switchButton.setOnCheckedChangeListener { _, isChecked ->
-
-            prefs?.edit()?.putBoolean(NOTIFICATION_PREFS,isChecked)?.apply()
+            val navController = findNavController(this)
+            prefs?.edit()?.putBoolean(NOTIFICATION_PREFS, isChecked)?.apply()
 
             if (isChecked) {
                 val options = TranslatorOptions.Builder()
@@ -58,6 +68,7 @@ class SettingsFragment : DialogFragment() {
                     .build()
 
                 val englishPortugueseTranslator = Translation.getClient(options)
+                lifecycle.addObserver(englishPortugueseTranslator)
 
                 var conditions = DownloadConditions.Builder()
                     .requireWifi()
@@ -66,18 +77,35 @@ class SettingsFragment : DialogFragment() {
                     .addOnSuccessListener {
                         // Model downloaded successfully. Okay to start translating.
                         // (Set a flag, unhide the translation UI, etc.)
+
                     }
                     .addOnFailureListener { exception ->
                         // Model couldn’t be downloaded or other internal error.
                         // ...
                     }
+                val alertDialog = AlertDialog.Builder(_view.context)
+                alertDialog.setTitle(getString(R.string.alerta_traducao))
+                alertDialog.setMessage(getString(R.string.message_traducao))
+                alertDialog.setPositiveButton(getString(R.string.sim)) { _, _ ->
+                    val bundle = bundleOf(
+                        "VALIDATION" to "isChecked"
+                    )
+                    navController.navigate(R.id.fatoAstronomicoFragment2, bundle)
+
+                }
+                alertDialog.setNegativeButton(getString(R.string.nao)) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                alertDialog.show()
+
 
             }
         }
 
-
     }
-    companion object{
+
+
+    companion object {
         const val APP_NAME = "switch_prefs"
         const val NOTIFICATION_PREFS = "NOTIFICATION_PREFS"
     }
