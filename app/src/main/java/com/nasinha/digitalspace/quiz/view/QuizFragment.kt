d4fb017046f2692804f9cbe0a89a53ba5bd04662
Mariana Marcelli <marimmarcelli@gmail.com>
2020-12-16 10:08:02 -0300

    Novas Telas Quiz - Início e Questões

--- app/src/main/java/com/nasinha/digitalspace/quiz/view/QuizFragment.kt
+++ app/src/main/java/com/nasinha/digitalspace/quiz/view/QuizFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.button.MaterialButton
 import com.nasinha.digitalspace.R
+import kotlinx.android.synthetic.main.fragment_quiz.*
 
 
 class QuizFragment : Fragment() {
@@ -30,8 +32,9 @@ class QuizFragment : Fragment() {
         view.findViewById<MaterialButton>(R.id.btnPlay).setOnClickListener {
             navController.navigate(R.id.action_quizFragment_to_quizQuestionsFragment)
         }
+        val quizToolBar = view.findViewById<MaterialToolbar>(R.id.quizTopAppBar)
 
-        view.findViewById<ImageButton>(R.id.btnBackQuiz).setOnClickListener {
+        quizToolBar.setNavigationOnClickListener {
             activity?.onBackPressed()
         }
 
