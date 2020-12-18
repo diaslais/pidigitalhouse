116ce7f9a24c170786178fec554ce6680482c15d
Renan <jrferreirad@gmail.com>
2020-12-18 10:39:14 -0300

    Adicionando media_type endpoint na apod

--- app/src/main/java/com/nasinha/digitalspace/apod/model/ApodResponseModel.kt
+++ app/src/main/java/com/nasinha/digitalspace/apod/model/ApodResponseModel.kt
@@ -1,9 +1,11 @@
 package com.nasinha.digitalspace.apod.model
 
 data class ApodResponseModel(
+
     val date: String,
-    val explanation:String,
-    val title:String,
-    val url:String
+    val explanation: String,
+    val title: String,
+    val url: String,
+    val media_type: String
 
 )
