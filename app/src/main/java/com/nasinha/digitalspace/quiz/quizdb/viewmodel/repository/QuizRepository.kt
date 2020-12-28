f0c283231013ce9b0fe73d8cdf4e44de6a821731
Mariana Marcelli <marimmarcelli@gmail.com>
2020-12-28 18:13:03 -0300

    Quiz

---
+++ app/src/main/java/com/nasinha/digitalspace/quiz/quizdb/viewmodel/repository/QuizRepository.kt
@@ -0,0 +1,10 @@
+package com.nasinha.digitalspace.quiz.quizdb.viewmodel.repository
+
+//import com.nasinha.digitalspace.quiz.quizdb.viewmodel.dao.QuestionsDao
+//import com.nasinha.digitalspace.quiz.quizdb.viewmodel.model.QuestionEntity
+
+//class QuizRepository (private val quizDao: QuestionsDao) {
+//
+//    suspend fun addQuestion(question : QuestionEntity) = quizDao.addQuestion(question)
+//
+//}
