2c0ac837090e2407e354be8f4546147ae2a16db5
Lais Dias <laisdias099@gmail.com>
2021-01-10 22:07:58 -0300

    refatora código, adiciona lógica do quiz, adiciona barra de timer no cronômetro

--- app/src/main/java/com/nasinha/digitalspace/quiz/view/QuestionsFragment.kt
+++ app/src/main/java/com/nasinha/digitalspace/quiz/view/QuestionsFragment.kt
@@ -1,40 +1,56 @@
 package com.nasinha.digitalspace.quiz.view
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.os.CountDownTimer
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.button.MaterialButton
 import com.nasinha.digitalspace.R
-import com.nasinha.digitalspace.quiz.data.Constants
-import com.nasinha.digitalspace.quiz.model.QuestionsModel
-import kotlinx.android.synthetic.main.fragment_questions.*
+import com.nasinha.digitalspace.quiz.db.QuizDatabase
+import com.nasinha.digitalspace.quiz.repository.QuizRepository
+import com.nasinha.digitalspace.quiz.viewmodel.QuizViewModel
 import java.util.*
 import kotlin.collections.ArrayList
 
 
 class QuestionsFragment : Fragment(), View.OnClickListener {
-
     private var _currentPosition: Int = 1
-    private var _questionsList: ArrayList<QuestionsModel>? = null
     private var _selectedOptionPosition: Int = 0
-    private lateinit var _view: View
     private var _correctAnswers: Int = 0
+    private var _isAnswered: Boolean = false //user selected an option
+    private var _goToNextQuestion: Boolean = false //user already clicked on "answer"
+
     private lateinit var countDownTimer: CountDownTimer
     private var timeLeftInMillis: Long = 0
-    private var countDownMillis: Long = 30000
+    private var barCounter: Int = 0
+    lateinit var barTimer: Timer
 
+    private lateinit var _view: View
+    private lateinit var _viewModel: QuizViewModel
+
+    lateinit var txtQuestion: TextView
+    lateinit var txtQuestionNumber: TextView
+    lateinit var txtOptionOne: TextView
+    lateinit var txtOptionTwo: TextView
+    lateinit var txtOptionThree: TextView
+    lateinit var txtOptionFour: TextView
+    lateinit var btnAnswer: MaterialButton
+    lateinit var txtChronometer: TextView
+    lateinit var imageClock: ImageView
+    lateinit var countdownBar: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,145 +63,78 @@ class QuestionsFragment : Fragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        _viewModel = ViewModelProvider(
+            this,
+            QuizViewModel.QuizViewModelFactory(QuizRepository(QuizDatabase.getDatabase(view.context).quizDao()))
+        ).get(QuizViewModel::class.java)
+
+        txtQuestion = view.findViewById(R.id.txtQuestion)
+        txtQuestionNumber = view.findViewById(R.id.txtQuestionNumber)
+        txtOptionOne = view.findViewById(R.id.txtAnswer1)
+        txtOptionTwo = view.findViewById(R.id.txtAnswer2)
+        txtOptionThree = view.findViewById(R.id.txtAnswer3)
+        txtOptionFour = view.findViewById(R.id.txtAnswer4)
+        btnAnswer = view.findViewById(R.id.btnNext)
+        txtChronometer = view.findViewById(R.id.txtChronometer)
+        imageClock = view.findViewById(R.id.imgClock)
+        countdownBar = view.findViewById(R.id.pbCountDown)
+
+        setQuestion()
+
         val quizToolBar = view.findViewById<MaterialToolbar>(R.id.quizTopAppBarQuestions)
 
         quizToolBar.setNavigationOnClickListener {
             activity?.onBackPressed()
         }
-        _questionsList = Constants.getQuestions()
 
-        setQuestion()
-
-        val optionOne = _view.findViewById<TextView>(R.id.btnAnswer1)
-        val optionTwo = _view.findViewById<TextView>(R.id.btnAnswer2)
-        val optionThree = _view.findViewById<TextView>(R.id.btnAnswer3)
-        val optionFour = _view.findViewById<TextView>(R.id.btnAnswer4)
-        val answerButton = _view.findViewById<MaterialButton>(R.id.btnNext)
-
-        optionOne.setOnClickListener(this)
-        optionTwo.setOnClickListener(this)
-        optionThree.setOnClickListener(this)
-        optionFour.setOnClickListener(this)
-        answerButton.setOnClickListener(this)
+        txtOptionOne.setOnClickListener(this)
+        txtOptionTwo.setOnClickListener(this)
+        txtOptionThree.setOnClickListener(this)
+        txtOptionFour.setOnClickListener(this)
+        btnAnswer.setOnClickListener(this)
     }
 
-    @SuppressLint("SetTextI18n")
+
     private fun setQuestion() {
+        _viewModel.questionsList.observe(viewLifecycleOwner) {
 
-        val question = _questionsList!![_currentPosition - 1]
+            val question = it[_currentPosition - 1]
+            defaultOptionsView()
+            startCountDown()
 
-        defaultOptionsView()
-        startCountDown()
-
-        val answerButton = _view.findViewById<MaterialButton>(R.id.btnNext)
-
-        if (_currentPosition == _questionsList!!.size) {
-            answerButton.text = "FIM"
-        } else {
-            answerButton.text = "Responder"
-        }
-
-        val questionNumber = _view.findViewById<TextView>(R.id.txtQuestionNumber)
-        questionNumber.text = "$_currentPosition" + "/" + _questionsList!!.size
-
-        val questionText = _view.findViewById<TextView>(R.id.txtQuestion)
-        questionText.text = question.question
-
-        val optionOne = _view.findViewById<TextView>(R.id.btnAnswer1)
-        val optionTwo = _view.findViewById<TextView>(R.id.btnAnswer2)
-        val optionThree = _view.findViewById<TextView>(R.id.btnAnswer3)
-        val optionFour = _view.findViewById<TextView>(R.id.btnAnswer4)
-
-        optionOne.text = question.answer1
-        optionTwo.text = question.answer2
-        optionThree.text = question.answer3
-        optionFour.text = question.answer4
-    }
-
-    private fun startCountDown() {
-        val optionOne = _view.findViewById<TextView>(R.id.btnAnswer1)
-        val optionTwo = _view.findViewById<TextView>(R.id.btnAnswer2)
-        val optionThree = _view.findViewById<TextView>(R.id.btnAnswer3)
-        val optionFour = _view.findViewById<TextView>(R.id.btnAnswer4)
-
-        countDownTimer = object : CountDownTimer(15000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                timeLeftInMillis = millisUntilFinished
-                val minutes = (timeLeftInMillis / 1000) / 60
-                val seconds = (timeLeftInMillis / 1000) % 60
-                val timeFormatted =
-                    String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
-
-                val countdown = _view.findViewById<TextView>(R.id.txtChronometer)
-                val imageClock = _view.findViewById<ImageView>(R.id.imageClock)
-
-                countdown.text = timeFormatted
-
-                if (seconds <= 10000) {
-                    changeCountDownColor()
-                }
-
-                if (seconds == 0L) {
-                    optionOne.isEnabled = false
-                    optionTwo.isEnabled = false
-                    optionThree.isEnabled = false
-                    optionFour.isEnabled = false
-                }
-            }
-
-            override fun onFinish() {
-                timeLeftInMillis = 0
-            }
-        }
-        countDownTimer.start()
-    }
-
-    private fun changeCountDownColor() {
-        val countdown = _view.findViewById<TextView>(R.id.txtChronometer)
-        val imageClock = _view.findViewById<ImageView>(R.id.imageClock)
-
-        if (timeLeftInMillis < 10000) {
-            imageClock.setColorFilter(_view.context.resources.getColor(R.color.colorIncorrectOption))
-            countdown.setTextColor(Color.parseColor("#FF3034"))
-        } else {
-            countdown.setTextColor(Color.parseColor("#FFFFFF"))
-            imageClock.setColorFilter(_view.context.resources.getColor(R.color.colorWhite))
+            btnAnswer.text = getString(R.string.Responder)
+            txtQuestionNumber.text = getString(R.string.contagem_questoes, _currentPosition, it.size)
+            txtQuestion.text = question.question
+            txtOptionOne.text = question.optionOne
+            txtOptionTwo.text = question.optionTwo
+            txtOptionThree.text = question.optionThree
+            txtOptionFour.text = question.optionFour
         }
     }
 
-    private fun answerQuestion(answer: Int, drawableView: Int) {
-        val optionOne = _view.findViewById<TextView>(R.id.btnAnswer1)
-        val optionTwo = _view.findViewById<TextView>(R.id.btnAnswer2)
-        val optionThree = _view.findViewById<TextView>(R.id.btnAnswer3)
-        val optionFour = _view.findViewById<TextView>(R.id.btnAnswer4)
-
+    private fun answerView(answer: Int, drawableView: Int) {
         when (answer) {
             1 -> {
-                optionOne.background = ContextCompat.getDrawable(_view.context, drawableView)
+                txtOptionOne.background = ContextCompat.getDrawable(_view.context, drawableView)
             }
             2 -> {
-                optionTwo.background = ContextCompat.getDrawable(_view.context, drawableView)
+                txtOptionTwo.background = ContextCompat.getDrawable(_view.context, drawableView)
             }
             3 -> {
-                optionThree.background = ContextCompat.getDrawable(_view.context, drawableView)
+                txtOptionThree.background = ContextCompat.getDrawable(_view.context, drawableView)
             }
             4 -> {
-                optionFour.background = ContextCompat.getDrawable(_view.context, drawableView)
+                txtOptionFour.background = ContextCompat.getDrawable(_view.context, drawableView)
             }
         }
     }
 
     private fun defaultOptionsView() {
-        val optionOne = _view.findViewById<TextView>(R.id.btnAnswer1)
-        val optionTwo = _view.findViewById<TextView>(R.id.btnAnswer2)
-        val optionThree = _view.findViewById<TextView>(R.id.btnAnswer3)
-        val optionFour = _view.findViewById<TextView>(R.id.btnAnswer4)
-
         val options = ArrayList<TextView>()
-        options.add(0, optionOne)
-        options.add(1, optionTwo)
-        options.add(2, optionThree)
-        options.add(3, optionFour)
+        options.add(0, txtOptionOne)
+        options.add(1, txtOptionTwo)
+        options.add(2, txtOptionThree)
+        options.add(3, txtOptionFour)
 
         for (option in options) {
             option.setTextColor(Color.parseColor("#FFFFFF"))
@@ -196,78 +145,80 @@ class QuestionsFragment : Fragment(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.btnAnswer1 -> {
-                selectedOption(btnAnswer1, 1)
-            }
-            R.id.btnAnswer2 -> {
-                selectedOption(btnAnswer2, 2)
-            }
-            R.id.btnAnswer3 -> {
-                selectedOption(btnAnswer3, 3)
-            }
-            R.id.btnAnswer4 -> {
-                selectedOption(btnAnswer4, 4)
-            }
-            R.id.btnNext -> {
-                if (btnNext.text == "Responder") {
-                    btnAnswer1.isEnabled = false
-                    btnAnswer2.isEnabled = false
-                    btnAnswer3.isEnabled = false
-                    btnAnswer4.isEnabled = false
-                    countDownTimer.cancel()
+        _viewModel.questionsList.observe(viewLifecycleOwner) {
+            val question = it[_currentPosition - 1]
+
+            when (v?.id) {
+                R.id.txtAnswer1 -> {
+                    selectedOption(txtOptionOne, 1)
                 }
-                if (_selectedOptionPosition == 0) {
-                    _currentPosition++
-                    when {
-                        _currentPosition <= _questionsList!!.size -> {
-                            setQuestion()
-                        }
-                        else -> {
-                            val navController = findNavController()
-                            val bundle = bundleOf(
-                                "CORRECT_ANSWERS" to _correctAnswers,
-                                "TOTAL_QUESTIONS" to _questionsList!!.size
-                            )
-                            if (_correctAnswers >= 5) {
-                                navController.navigate(
-                                    R.id.action_questionsFragment_to_quizScoreFragment, bundle
+                R.id.txtAnswer2 -> {
+                    selectedOption(txtOptionTwo, 2)
+                }
+                R.id.txtAnswer3 -> {
+                    selectedOption(txtOptionThree, 3)
+                }
+                R.id.txtAnswer4 -> {
+                    selectedOption(txtOptionFour, 4)
+                }
+                R.id.btnNext -> {
+                    if (_goToNextQuestion) { //second click
+                        _currentPosition++
+                        _goToNextQuestion = false
+                        _isAnswered = false
+                        when {
+                            _currentPosition <= it.size -> {
+                                setQuestion()
+                            }
+                            else -> {
+                                val navController = findNavController()
+                                val bundle = bundleOf(
+                                    "CORRECT_ANSWERS" to _correctAnswers,
+                                    "TOTAL_QUESTIONS" to it.size
                                 )
-                            } else {
-                                navController.navigate(
-                                    R.id.action_questionsFragment_to_quizScoreLostFragment, bundle
-                                )
+                                if (_correctAnswers >= 5) {
+                                    navController.navigate(
+                                        R.id.action_questionsFragment_to_quizScoreFragment, bundle
+                                    )
+                                } else {
+                                    navController.navigate(
+                                        R.id.action_questionsFragment_to_quizScoreLostFragment, bundle
+                                    )
+                                }
                             }
                         }
-                    }
-                } else {
-                    val question = _questionsList?.get(_currentPosition - 1)
+                        optionsSwitch()
+                    } else if (_isAnswered) { //first click
+                        stopTimer()
+                        _goToNextQuestion = true
+                        if (question.correctAnswer != _selectedOptionPosition) {
+                            answerView(_selectedOptionPosition, R.drawable.incorrect_question)
+                        } else {
+                            _correctAnswers++
+                        }
+                        question.correctAnswer?.let { answerView(it, R.drawable.correct_question) }
 
-                    if (question!!.correctAnswer != _selectedOptionPosition) {
-                        answerQuestion(_selectedOptionPosition, R.drawable.incorrect_question)
-                    } else {
-                        _correctAnswers++
+                        if (_currentPosition == it.size) {
+                            btnAnswer.text = getString(R.string.fim)
+                        } else {
+                            btnAnswer.text = getString(R.string.proxima_questao)
+                        }
+                        optionsSwitch()
                     }
-                    answerQuestion(question.correctAnswer, R.drawable.correct_question)
-
-                    if (_currentPosition == _questionsList!!.size) {
-                        btnNext.text = "FIM"
-                        countDownTimer.cancel()
-                        btnAnswer1.isEnabled = false
-                        btnAnswer2.isEnabled = false
-                        btnAnswer3.isEnabled = false
-                        btnAnswer4.isEnabled = false
-                        countDownTimer.cancel()
-                    } else {
-                        btnNext.text = "Próxima Questão"
-                    }
-                    _selectedOptionPosition = 0
                 }
             }
         }
     }
 
+    private fun optionsSwitch() {
+        txtOptionOne.isEnabled = !(txtOptionOne.isEnabled)
+        txtOptionTwo.isEnabled = !(txtOptionTwo.isEnabled)
+        txtOptionThree.isEnabled = !(txtOptionThree.isEnabled)
+        txtOptionFour.isEnabled = !(txtOptionFour.isEnabled)
+    }
+
     private fun selectedOption(tv: TextView, selectedOptionNumber: Int) {
+        _isAnswered = true
         defaultOptionsView()
         _selectedOptionPosition = selectedOptionNumber
 
@@ -276,6 +227,61 @@ class QuestionsFragment : Fragment(), View.OnClickListener {
         tv.background = ContextCompat.getDrawable(_view.context, R.drawable.selected_question)
     }
 
+    private fun startCountDown() {
+        startCountDownBar()
+        countDownTimer = object : CountDownTimer(15000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeftInMillis = millisUntilFinished
+                val minutes = (timeLeftInMillis / 1000) / 60
+                val seconds = (timeLeftInMillis / 1000) % 60
+                val timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
+
+                txtChronometer.text = timeFormatted
+
+                if (seconds <= 5000) {
+                    changeCountDownColor()
+                }
+
+                if (seconds == 0L) {
+                    optionsSwitch()
+                }
+            }
+
+            override fun onFinish() {
+                timeLeftInMillis = 0
+            }
+        }
+        countDownTimer.start()
+    }
+
+    private fun startCountDownBar() {
+        barTimer = Timer()
+        val barTimerTask = object : TimerTask() {
+            override fun run() {
+                barCounter++
+                countdownBar.setProgress(barCounter)
+                if (barCounter == 100) {
+                    stopTimer()
+                    btnAnswer.isEnabled = false
+                }
+            }
+        }
+        barTimer.schedule(barTimerTask, 0, 150)
+    }
+
+    private fun stopTimer() {
+        barTimer.cancel()
+        countDownTimer.cancel()
+        barCounter = 0
+    }
+
+    private fun changeCountDownColor() {
+        if (timeLeftInMillis <= 5000) {
+            txtChronometer.setTextColor(Color.parseColor("#FF3034"))
+        } else {
+            txtChronometer.setTextColor(Color.parseColor("#FFFFFF"))
+        }
+    }
 }
 
 
