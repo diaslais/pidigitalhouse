705b4244c40383120d352dceb560dbf10f294124
Lais Dias <laisdias099@gmail.com>
2021-01-21 12:23:47 -0300

    implementa layout tela de pontuacao e adiciona link no drawer

---
+++ app/src/main/java/com/nasinha/digitalspace/quiz/view/ScoreFragment.kt
@@ -0,0 +1,36 @@
+package com.nasinha.digitalspace.quiz.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import com.nasinha.digitalspace.R
+
+class ScoreFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_score, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnBack = view.findViewById<ImageButton>(R.id.btnBackScore)
+
+        btnBack.setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+    }
+
+}
