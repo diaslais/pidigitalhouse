250e0586714deea9f3babe43bb66e260705e9f40
Renan <jrferreirad@gmail.com>
2021-02-06 16:23:36 -0300

    sobe versão 1.0.2 da aplicação

--- app/src/main/java/com/nasinha/digitalspace/developer/db/AppDatabase.kt
+++ app/src/main/java/com/nasinha/digitalspace/developer/db/AppDatabase.kt
@@ -9,7 +9,7 @@ import com.nasinha.digitalspace.developer.entity.DeveloperEntity
 
 @Database(
     entities = [DeveloperEntity::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
