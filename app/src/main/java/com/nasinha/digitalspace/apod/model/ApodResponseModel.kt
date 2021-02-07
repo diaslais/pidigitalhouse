6b73b3aacec4b46170914e710feb54af329f2a72
Renan <jrferreirad@gmail.com>
2021-02-07 00:04:32 -0300

    resolve bug da requisição de api em release

--- app/src/main/java/com/nasinha/digitalspace/apod/model/ApodResponseModel.kt
+++ app/src/main/java/com/nasinha/digitalspace/apod/model/ApodResponseModel.kt
@@ -1,11 +1,17 @@
 package com.nasinha.digitalspace.apod.model
 
-data class ApodResponseModel(
+import com.google.gson.annotations.SerializedName
 
+data class ApodResponseModel(
+    @SerializedName("date")
     val date: String,
+    @SerializedName("explanation")
     val explanation: String,
+    @SerializedName("title")
     val title: String,
+    @SerializedName("url")
     val url: String,
+    @SerializedName("media_type")
     val media_type: String
 
 )
--- app/src/main/java/com/nasinha/digitalspace/epic/model/EpicResponseModel.kt
+++ app/src/main/java/com/nasinha/digitalspace/epic/model/EpicResponseModel.kt
@@ -1,6 +1,10 @@
 package com.nasinha.digitalspace.epic.model
 
+import com.google.gson.annotations.SerializedName
+
 data class EpicResponseModel (
+    @SerializedName("caption")
     val caption:String,
+    @SerializedName("image")
     val image:String
 )
